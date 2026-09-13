383373a339a6273ab13ad1b0b2a1aa475721eb18
jacobkorsgaard <jacob461d@gmail.com>
2026-09-13 23:49:51 +0200

    En del logik indpakket i metoder

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 public class Main {
 
-    void main(){
 
-        // DELCLERATIONS
-        String[] levelDescription = {"Level 1: Unknown - Producing on a cheap laptop in your bedroom", "Level 2: Beginner - Showing off your noob songs on SoundCloud", "Level 3: Intermediate - Slowly building a fanbase", "Level 4: Established - You have a small, but supportive fanbase", "Level 5: Rising - Indie record labels are now starting to take notice", "Level 6: Signed artist - You are now signed at a small record label", "Level 7: High momentum - Your songs are performing well on Spotify", "Level 8: Viral artist - One of your songs went viral on TikTok", "Level 9: Bigger and better - A major record label noticed your potential", "Level 10: Rising star - the label offered a contract", "Level 11: Tomorrowland - You played on a smaller stage at Tomorrowland", "Level 12: Managed artist - Label hired you a manager", "Level 13: Mainstage beginner - You are now playing mainstage at festivals", "Level 14: Mainstage intermediate - More people appear at your shows", "Level 15: Mainstage prodigy - People now show up at festivals specifically for you", "Level 16: God status - Your name now appears alongside artists who used to be your idols", "Level 17: B2B - You are playing B2B with names like David Guetta", "Level 18: Billboard Top 1: Your song hits #1 on Billboard", "Level 19: Spotify Top 1 - Your song sits at #1 on Spotify for 5 concurrent months", "Level 20: International - 99% of the world knows your face, even in rural towns", "Level 21: Best DJ/Producer - You are considered the best musician in recorded time, and manage to win every single award in every single category", "Level 22: - Final level. An unreal amount of fame. You are considered a god in most peoples eyes. Even aliens in distant galaxies consider you their god. Your music is discovered to heal every illness. you are chosen to rule the entire universe. Even Donald Trump bows down when he sees you."};
+    // VARIABLES / DECLARATIONS
+    String bandName = "Talvo";
+    int fans = 8500;
+    int maxFans = 15000;
+    int fameLevel = 2;
+    int xp = 3200;
+    double cash = 4750.50;
+    boolean isActive = true;
+    char genre = 'E';
+    String[] repertoire = {"REACT", "Low", "Sweet Dreams", "Freestyler"};
 
-
-
-
-        // Band: Talvo
-        String bandName = "Talvo";
-        int fans = 40000;
-        int maxFans = 50000;
-        int fameLevel = 4;
-        int xp = 400;
-        double cash = 2500.0;
-        boolean isActive = true;
-        char genre = 'E';
-
-        String[] songs = {"REACT", "Low", "Sweet Dreams", "Freestyler"};
-
-        System.out.println("/// BAND PROFILE TALVO ///");
+    //METHODS
+    void printBandProfile(){
+        System.out.println("=== BAND PROFILE: TALVO ===");
         System.out.println("Name: " + bandName);
         System.out.println("Genre: " + genre);
         System.out.println("Fame Level: " + fameLevel);
@@ -28,21 +22,151 @@ public class Main {
         System.out.println(xp);
         System.out.println("Money: " + cash);
         System.out.println("Active: " + isActive);
+        System.out.println();
+    }
 
-        System.out.println(" ");
+    // GIG SIMULATION
+    void playGig(int venueCapacity, int attendance){
+        System.out.println("=== CONCERT ===");
+        if(attendance >= venueCapacity * 0.80){
+            fans += 200;
+            System.out.println("Attendance over 80% of capacity. You gained 200 fans.");
+            System.out.println("Your total amount of fans is now: " + fans);
+        }
+        else{
+            fans += 50;
+            System.out.println("Attendance under 80% of capacity. You gained 50 fans.");
+            System.out.println("Your total amount of fans is now: " + fans);
+        }
+        cash += 1500.0;
+        System.out.println("You were paid +1500$ for the show");
+        System.out.println("Your total cash balance is now: " + cash);
+        System.out.println();
+    }
+
+    //GAIN FANS
+    void gainFans(int fansToAdd){
+        fans += fansToAdd;
+        System.out.println("Gained " + fansToAdd + " fans. New total: " + fans);
+    }
+
+    //LOSE FANS
+    void loseFans(int fansToRemove){
+        fans -= fansToRemove;
+        System.out.println("Lost " + fansToRemove + "fans. New total: " + fans);
+    }
+
+    //EARN MONEY
+    void earnMoney(double amount){
+        cash += amount;
+        System.out.println("Earned: " + amount + "$. New cash balance: " + cash);
+        return;
+    }
+
+    //SPEND MONEY --------------------- MANGLER RETURN --------------------- note til mig selv: vender tilbage til senere
+    void spendMoney(double amount) {
+        if (amount <= cash) {
+            cash -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    // ADD XP
+    void addXP(int amount){
+        xp += amount;
+        System.out.println(amount + "XP gained. Checking for level up...");
+        if(xp > (2000 * fameLevel)){
+            System.out.println("Ready to level up!");
+            levelUp();
+        }
+        else{
+            System.out.println("Not ready for level up...");
+        }
+    }
+
+    // LEVEL UP
+    void levelUp(){
+        if(fameLevel != 5){
+            fameLevel += 1;
+            xp = 0;
+            System.out.println("==============================");
+            System.out.println("Leveled up! Your new level is:");
+            levelChecker();
+            System.out.println("==============================");
+        }
+    }
+
+    //LEVEL CHECKER
+    void levelChecker(){
+        switch(fameLevel){
+            case 1:
+                System.out.println("Level 1: Unknown - Producing music in your bedroom");
+                break;
+            case 2:
+                System.out.println("Level 2: Local DJ - Playing small club gigs");
+                break;
+            case 3:
+                System.out.println("Level 3: Rising star - Festival invitations coming in");
+                break;
+            case 4:
+                System.out.println("Level 4: Mainstream - Tomorrowland is calling");
+                break;
+            case 5:
+                System.out.println("Level 5: Most respected DJ - Even David Guetta looks up to you");
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+    }
+
+    // RELEVANCE CHECK--------------------- MANGLER RETURN --------------------- note til mig selv: vender tilbage til senere
+    void isLosingRelevance(){
+
+    }
+
+
+    // ACTIVITY CHECK --------------------- MANGLER RETURN --------------------- note til mig selv: vender tilbage til senere
+    void isActive(){
+
+
+    }
+
+
+
+
+
+
+
+
+    void main(){
+
+
+
+
+
+
+
+        printBandProfile();
+
+
+
+
+
 
         System.out.println("Repertoire: (4 songs)");
-        System.out.println("- " + songs[0]);
-        System.out.println("- " + songs[1]);
-        System.out.println("- " + songs[2]);
-        System.out.println("- " + songs[3]);
-        System.out.println(" ");
+        for(int i = 0; i < repertoire.length; i++){
+            System.out.println("- " + repertoire[i]);
+        }
+        System.out.println();
 
 
 
 
 
 
+        /*
         // Rival band: BEAUZ
         String rivalBand = "BEAUZ";
         int rivalFans = 220000;
@@ -73,143 +197,81 @@ public class Main {
         System.out.println("- " + rivalSongs[3]);
         System.out.println(" ");
 
-
-        // STATUS CHECK LOGIC
-
-        //ROCK
-        if(fans < maxFans * 0.25 & genre == 'R'){
-            System.out.println("WARNING: Rock band losing relevance! Consider a comeback strategy.");
-        }
-
-        if(fans <= 0 & genre == 'R'){
-            isActive = false;
-            System.out.println("The rock band has broken up...");
-        }
-
-        //ELECTRONIC
-        if(fans < maxFans * 0.25 & genre == 'E'){
-            System.out.println("WARNING: Electronic band losing relevance! Consider a comeback strategy.");
-        }
-
-        if(fans <= 0 & genre == 'E'){
-            isActive = false;
-            System.out.println("The electronic band has broken up...");
-        }
+         */
 
 
-        //HIP HOP
-        if(fans < maxFans * 0.25 & genre == 'H'){
-            System.out.println("WARNING: Hip Hop band losing relevance! Consider a comeback strategy.");
+
+        // BAND LOGIK
+
+        //STATUS CHECKS
+
+        System.out.println("=== STATUS CHECK ===");
+        if(fans < (maxFans * 0.75)){
+            System.out.println("WARNING: Losing relevance! Consider a comeback strategy");
         }
 
-        if(fans <= 0 & genre == 'H'){
-            isActive = false;
-            System.out.println("The Hip Hop band has broken up...");
+        if(fans <= 0){
+            System.out.println("The band has broken up…");
         }
+        System.out.println();
 
-        //POP
-        if(fans < maxFans * 0.25 & genre == 'P'){
-            System.out.println("WARNING: Hip Hop band losing relevance! Consider a comeback strategy.");
-        }
+        System.out.println("=== MENTALITY ===");
+        switch(genre){
+            case 'R':
+                System.out.println("Rock 'n' Roll!");
+                break;
+            case 'E':
+                System.out.println("Aiming for Tomorrowland!");
+                break;
+            case 'H':
+                System.out.println("Making waves in the underground!");
+                break;
+            case 'P':
+                System.out.println("Waiting for the next radio hit!");
+                break;
+            default:
+                System.out.println("Error");
+                break;
 
-        if(fans <= 0 & genre == 'P'){
-            isActive = false;
-            System.out.println("The Hip Hop band has broken up...");
         }
+        System.out.println();
 
 
-        // FAME SYSTEM
-
-        if(xp > 1000 * fameLevel){
-            System.out.println("Ready to level up!");
-        }
 
 
-        if(fameLevel == 1){
-            System.out.println(levelDescription[0]);
+        // REPERTOIRE
+        int songCounter = 0;
+        System.out.println("=== REPERTOIRE ===");
+        for(int i = 0; i < repertoire.length; i++){
+            System.out.println("- " + repertoire[i]);
+            songCounter++;
         }
-        else if(fameLevel == 2){
-            System.out.println(levelDescription[1]);
-        }
-        else if(fameLevel == 3){
-            System.out.println(levelDescription[2]);
-        }
-        else if(fameLevel == 4){
-            System.out.println(levelDescription[3]);
-        }
-        else if(fameLevel == 5){
-            System.out.println(levelDescription[4]);
-        }
-        else if(fameLevel == 6){
-            System.out.println(levelDescription[5]);
-        }
-        else if(fameLevel == 7){
-            System.out.println(levelDescription[6]);
-        }
-        else if(fameLevel == 8){
-            System.out.println(levelDescription[7]);
-        }
-        else if(fameLevel == 9){
-            System.out.println(levelDescription[8]);
-        }
-        else if(fameLevel == 10){
-            System.out.println(levelDescription[9]);
-        }
-        else if(fameLevel == 11){
-            System.out.println(levelDescription[10]);
-        }
-        else if(fameLevel == 12){
-            System.out.println(levelDescription[11]);
-        }
-        else if(fameLevel == 13){
-            System.out.println(levelDescription[12]);
-        }
-        else if(fameLevel == 14){
-            System.out.println(levelDescription[13]);
-        }
-        else if(fameLevel == 15){
-            System.out.println(levelDescription[14]);
-        }
-        else if(fameLevel == 16){
-            System.out.println(levelDescription[15]);
-        }
-        else if(fameLevel == 17){
-            System.out.println(levelDescription[16]);
-        }
-        else if(fameLevel == 18){
-            System.out.println(levelDescription[17]);
-        }
-        else if(fameLevel == 19){
-            System.out.println(levelDescription[18]);
-        }
-        else if(fameLevel == 20){
-            System.out.println(levelDescription[19]);
-        }
-        else if(fameLevel == 21){
-            System.out.println(levelDescription[20]);
-        }
-        else if(fameLevel == 22){
-            System.out.println(levelDescription[21]);
-        }
-        else{
-            System.out.println("An error occured...");
-        }
-
-        // REPERTOIRE SYSTEM
-        System.out.println(" ");
-        System.out.println("Repertoire:");
-        for (int i = 0; i < songs.length; i++){
-            System.out.println(songs[i]);
-        }
-        System.out.println("Total songs: " + songs.length);
-
+        System.out.println();
+        System.out.println("=== TOTAL SONGS ===");
+        System.out.println(songCounter);
+        System.out.println();
 
         // GIG SIMULATION
-        int venueCapacity = 500;
-        int attendance = 420;
-        double attendancePercent = venueCapacity / attendance;
-        System.out.println((double)attendancePercent);
+        playGig(500, 420);
 
+
+        //RANDOM EVENT
+        System.out.println("=== RANDOM EVENT ===");
+        int eventType = 1;
+
+        if(eventType == 1){
+            fans += 500;
+            System.out.println("Amazing show in Ibiza!: +500 fans");
+            System.out.println("You now have: " + fans + " fans");
+        }
+        else if(eventType == 2){
+            System.out.println("Been sick all week. No progress");
+        }
+        else{
+            fans -= 100;
+            System.out.println("Scandal! Speakers disconnected for an entire show: -300 fans");
+            System.out.println("You now have: " + fans + " fans");
+        }
 
 
 
